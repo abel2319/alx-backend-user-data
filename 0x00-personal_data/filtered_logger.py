69df67a74b3c8dda0e64dc4d8dd4a0acbcb3835b
abel2319 <abel.kpohinto@imsp-uac.org>
2023-11-03 13:19:56 +0100

    task0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 """0. Regex-ing
 """
+import re
+from typing import List
 
 
-def filter_datum():
-    """_summary_
-    """
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
     
