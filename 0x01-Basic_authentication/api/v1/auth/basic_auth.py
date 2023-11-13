e3000822a8677603edb9ea8bbbe0bc908056e3b5
abel2319 <abelbakary@gmail.com>
2023-11-14 00:55:15 +0100

    feat(task8):task 8 ✅

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """6. Basic auth
 """
 from api.v1.auth.auth import Auth
+import base64
 from flask import request
 from typing import List, TypeVar
 
@@ -20,3 +21,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """Decode base64 authorization header
+        """
+        if not base64_authorization_header or\
+            not isinstance(base64_authorization_header, str):
+            return None
+        
+        try:
+            b64 = base64_authorization_header.encode('utf-8')
+            return base64.b64decode(b64).decode('utf-8')
+        except Exception:
+            return
