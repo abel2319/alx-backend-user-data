b5e272159be5ae9b1ec3e9edb6b296eede5742ef
abel2319 <abelbakary@gmail.com>
2023-11-14 02:15:55 +0100

    feat(task10):task 10 ✅

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 from api.v1.auth.auth import Auth
 import base64
 from flask import request
+from models.user import User
 from typing import List, TypeVar
 
 
@@ -50,3 +51,22 @@ class BasicAuth(Auth):
 
         return (decoded_base64_authorization_header.split(':')[0],
                 decoded_base64_authorization_header.split(':')[1])
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on 
+        his email and password.
+        """
+        if not user_email or not isinstance(user_email, str) or\
+            not user_pwd or not isinstance(user_pwd, str):
+            return None
+
+        usr = User.search({"email": user_email})
+        if not usr or len(usr) == 0:
+            return None
+
+        if not usr[0].is_valid_password(user_pwd):
+            return None
+        
+        return usr[0]
