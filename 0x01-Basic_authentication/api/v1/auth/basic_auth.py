915d4d961bd50114c76e803f07f96619f489589a
abel2319 <abelbakary@gmail.com>
2023-11-14 01:03:58 +0100

    feat(task9):task 9 ✅

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,3 +36,17 @@ class BasicAuth(Auth):
             return base64.b64decode(b64).decode('utf-8')
         except Exception:
             return
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """returns the user email and password from
+        the Base64 decoded value.
+        """
+        if not decoded_base64_authorization_header or\
+            not isinstance(decoded_base64_authorization_header, str) or\
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        return (decoded_base64_authorization_header.split(':')[0],
+                decoded_base64_authorization_header.split(':')[1])
