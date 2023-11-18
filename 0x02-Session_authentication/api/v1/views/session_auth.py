42c0c7bbaaf626f85f77c9ce0b7a268510055bed
abel2319 <abelbakary@gmail.com>
2023-11-18 17:30:13 +0100

    fix(task5): task5 🩹 pycodestyle

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
+from os import getenv
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
-from os import getenv
 
 
 @app_views.route('/auth_session/login', methods='POST', strict_slashes=False)
@@ -13,20 +13,20 @@ def login(request):
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    
-    if not email or email == '':
-       return jsonify({ "error": "email missing" }), 400
 
-    if  not password and password == '':
-        return jsonify({ "error": "password missing" }), 400
-    
+    if not email or email == '':
+        return jsonify({"error": "email missing"}), 400
+
+    if not password and password == '':
+        return jsonify({"error": "password missing"}), 400
+
     user = User.search(email)
     if not user:
-        return jsonify({ "error": "no user found for this email" }), 404
-    
+        return jsonify({"error": "no user found for this email"}), 404
+
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
-    
+        return jsonify({"error": "wrong password"}), 401
+
     from api.v1.app import auth
     session_id = auth.create_session(user.get(id))
     res = jsonify(user.to_json())
