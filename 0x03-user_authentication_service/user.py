ceaccc38d1304281e957f3402955dcc18d05969a
abel2319 <abelbakary@gmail.com>
2023-12-05 05:18:05 +0100

    fix(task1): task1 🩹

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,7 +13,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(256), nullable=False)
-    hashed_password = Column(String(256), nullable=False)
-    session_id = Column(String(256), nullable=False)
-    reset_token = Column(String(256), nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
